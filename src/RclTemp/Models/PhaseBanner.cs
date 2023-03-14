26b448b2ee4bdc3b6b6b4295751b7939e6a10772
Phil Davies <github@phildavies.co.uk>
2023-03-14 10:56:21 +0000

    phase banner wip

---
+++ src/RclTemp/Models/PhaseBanner.cs
@@ -0,0 +1,21 @@
+﻿
+namespace RclTemp.Models;
+
+public enum Phase
+{
+    Alpha,
+    Beta
+}
+
+public class PhaseBanner
+{
+    public Phase Phase { get; }
+
+    public string FeedbackUrl { get; }
+
+    public PhaseBanner(string feedbackUrl, Phase phase = Phase.Beta)
+    {
+        Phase = phase;
+        FeedbackUrl = feedbackUrl;
+    }
+}
