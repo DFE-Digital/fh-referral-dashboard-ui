826112a08f9ff00f25936ef24637d50ebcc6599e
Phil Davies <github@phildavies.co.uk>
2023-03-21 11:07:50 +0000

    wip

--- src/RclTemp/FamilyHubsUi/Options/LinkOptions.cs
+++ src/RclTemp/FamilyHubsUi/Options/LinkOptions.cs
@@ -1,8 +1,10 @@
-﻿
+﻿using System.ComponentModel.DataAnnotations;
+
 namespace RclTemp.FamilyHubsUi.Options;
 
 public class LinkOptions
 {
+    [Required]
     public string Text { get; set; } = "";
     public string? Url { get; set; }
     public string? ConfigUrl { get; set; }
