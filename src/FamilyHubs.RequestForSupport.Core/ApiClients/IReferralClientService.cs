18525b36db754eeb1e9cb178291bb16b79aba839
Phil Davies <github@phildavies.co.uk>
2023-06-22 12:14:24 +0100

    wip

---
+++ src/FamilyHubs.RequestForSupport.Core/ApiClients/IReferralClientService.cs
@@ -0,0 +1,21 @@
+﻿using FamilyHubs.ReferralService.Shared.Dto;
+using FamilyHubs.ReferralService.Shared.Enums;
+using FamilyHubs.ReferralService.Shared.Models;
+
+namespace FamilyHubs.RequestForSupport.Core.ApiClients;
+
+public enum ReferralStatus
+{
+    New = 1,
+    Opened,
+    Accepted,
+    Declined
+}
+
+public interface IReferralClientService
+{
+    Task<PaginatedList<ReferralDto>> GetRequestsForConnectionByOrganisationId(string organisationId, ReferralOrderBy? orderBy, bool? isAscending, int pageNumber = 1, int pageSize = 10);
+    Task<PaginatedList<ReferralDto>> GetRequestsByLaProfessional(string accountId, ReferralOrderBy? orderBy, bool? isAscending, int pageNumber = 1, int pageSize = 10, CancellationToken cancellationToken = default);
+    Task<ReferralDto> GetReferralById(long referralId);
+    Task<string> UpdateReferralStatus(long referralId, ReferralStatus referralStatus, string? reason = null);
+}
--- src/FamilyHubs.RequestForSupport.Core/ApiClients/ReferralClientService.cs
+++ src/FamilyHubs.RequestForSupport.Core/ApiClients/ReferralClientService.cs
@@ -1,26 +1,9 @@
 ﻿using FamilyHubs.ReferralService.Shared.Dto;
 using FamilyHubs.ReferralService.Shared.Enums;
 using FamilyHubs.ReferralService.Shared.Models;
-using System.Text;
 using System.Text.Json;
 
 namespace FamilyHubs.RequestForSupport.Core.ApiClients;
-
-public enum ReferralStatus
-{
-    New = 1,
-    Opened,
-    Accepted,
-    Declined
-}
-
-public interface IReferralClientService
-{
-    Task<PaginatedList<ReferralDto>> GetRequestsForConnectionByOrganisationId(string organisationId, ReferralOrderBy? orderBy, bool? isAscending, int pageNumber = 1, int pageSize = 10);
-    Task<PaginatedList<ReferralDto>> GetRequestsByLaProfessional(string accountId, ReferralOrderBy? orderBy, bool? isAscending, int pageNumber = 1, int pageSize = 10, CancellationToken cancellationToken = default);
-    Task<ReferralDto> GetReferralById(long referralId);
-    Task<string> UpdateReferralStatus(long referralId, ReferralStatus referralStatus, string? reason = null);
-}
 
 public class ReferralClientService : ApiService, IReferralClientService
 {
