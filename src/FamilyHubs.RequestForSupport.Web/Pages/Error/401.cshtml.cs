13a2632e7755c1850630a9989f3fe7f5cba900bc
Phil Davies <github@phildavies.co.uk>
2023-07-17 15:28:33 +0100

    update packages and links wip

--- src/FamilyHubs.RequestForSupport.Web/Pages/Error/401.cshtml.cs
+++ src/FamilyHubs.RequestForSupport.Web/Pages/Error/401.cshtml.cs
@@ -1,11 +1,12 @@
 using FamilyHubs.SharedKernel.Razor.FamilyHubsUi.Delegators;
+using FamilyHubs.SharedKernel.Razor.FamilyHubsUi.Options;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace FamilyHubs.RequestForSupport.Web.Pages.Error;
 
 public class Error401Model : PageModel, IFamilyHubsHeader
 {
-    LinkStatus IFamilyHubsHeader.GetStatus(SharedKernel.Razor.FamilyHubsUi.Options.LinkOptions link)
+    LinkStatus IFamilyHubsHeader.GetStatus(FhLinkOptions link)
     {
         return link.Text == "Received requests" ? LinkStatus.Active : LinkStatus.Visible;
     }
