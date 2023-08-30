a0f59ed4d84afa930d5a8d0b12048107aa9a4900
Phil Davies <github@phildavies.co.uk>
2023-08-30 10:29:39 +0100

    wip

--- src/FamilyHubs.RequestForSupport.Web/Pages/Shared/HeaderPageModel.cs
+++ src/FamilyHubs.RequestForSupport.Web/Pages/Shared/HeaderPageModel.cs
@@ -8,7 +8,6 @@ namespace FamilyHubs.RequestForSupport.Web.Pages.Shared;
 
 public class HeaderPageModel : PageModel, IFamilyHubsHeader
 {
-    //todo: we could add the status to IFhRenderLink and let the consumer use either method
     IEnumerable<IFhRenderLink> IFamilyHubsHeader.NavigationLinks(
         FhLinkOptions[] navigationLinks,
         IFamilyHubsUiOptions familyHubsUiOptions)
@@ -16,22 +15,7 @@ public class HeaderPageModel : PageModel, IFamilyHubsHeader
         string role = HttpContext.GetRole();
 
         return role is RoleTypes.VcsProfessional or RoleTypes.VcsDualRole
-            ? navigationLinks.Select(nl => nl.Text == "Requests sent"
-                ? (IFhRenderLink)new FhRenderLink("My requests")
-                {
-                    Url = nl.Url,
-                    OpenInNewTab = nl.OpenInNewTab
-                } : nl)
+            ? familyHubsUiOptions.GetAlternative("VcsHeader").Header.NavigationLinks
             : navigationLinks;
-
-        //or
-        //return role is RoleTypes.VcsProfessional or RoleTypes.VcsDualRole
-        //    ? familyHubsUiOptions.GetAlternative("VcsHeader").Header.NavigationLinks
-        //    : navigationLinks;
-    }
-
-    LinkStatus IFamilyHubsHeader.GetStatus(IFhRenderLink link)
-    {
-        return link.Text is "Requests sent" or "My requests" ? LinkStatus.Active : LinkStatus.Visible;
     }
 }
