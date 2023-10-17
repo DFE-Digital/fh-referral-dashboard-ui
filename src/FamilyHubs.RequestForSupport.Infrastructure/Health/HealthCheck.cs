fc4fe0c6af5ea00a4ca716d481e2151e2d0fd174
Phil Davies <github@phildavies.co.uk>
2023-10-17 15:31:45 +0100

    tidy up

--- src/FamilyHubs.RequestForSupport.Infrastructure/Health/HealthCheck.cs
+++ src/FamilyHubs.RequestForSupport.Infrastructure/Health/HealthCheck.cs
@@ -1,8 +1,6 @@
 ﻿using Azure.Core;
 using Azure.Identity;
-using HealthChecks.UI.Client;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using FamilyHubs.SharedKernel.Razor.Health;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
@@ -11,44 +9,6 @@ namespace FamilyHubs.RequestForSupport.Infrastructure.Health;
 
 public static class HealthCheck
 {
-    public enum UrlType
-    {
-        InternalApi,
-        ExternalApi,
-        InternalSite,
-        ExternalSite
-    }
-
-    public static IHealthChecksBuilder AddApi(
-        this IHealthChecksBuilder builder,
-        string name,
-        string configKey,
-        IConfiguration configuration,
-        UrlType urlType = UrlType.InternalApi)
-    {
-        string? apiUrl = configuration.GetValue<string>(configKey);
-
-        // Only add the health check if the config key is set.
-        // Either the API is optional (or not used locally) and missing intentionally,
-        // in which case there's no need to add the health check,
-        // or it's required, but in that case, the real consumer of the API should
-        // continue to throw it's own relevant exception
-        if (!string.IsNullOrEmpty(apiUrl))
-        {
-            if (urlType == UrlType.InternalApi)
-            {
-                //todo: add "/health" endpoints to all APIs
-                apiUrl = apiUrl.TrimEnd('/') + "/api/info";
-            }
-
-            // we handle API failures as Degraded, so that App Services doesn't remove or replace the instance (all instances!) due to an API being down
-            builder.AddUrlGroup(new Uri(apiUrl), name, HealthStatus.Degraded,
-                new[] { urlType.ToString() });
-        }
-
-        return builder;
-    }
-
     public static IServiceCollection AddSiteHealthChecks(
         this IServiceCollection services,
         IConfiguration config)
@@ -73,7 +33,7 @@ public static class HealthCheck
 
         // we handle API failures as Degraded, so that App Services doesn't remove or replace the instance (all instances!) due to an API being down
         var healthCheckBuilder = services.AddFamilyHubsHealthChecks(config)
-            .AddIdentityServer(new Uri(oneLoginUrl!), name: "One Login", failureStatus: HealthStatus.Degraded, tags: new[] { UrlType.ExternalApi.ToString() })
+            .AddIdentityServer(new Uri(oneLoginUrl!), name: "One Login", failureStatus: HealthStatus.Degraded, tags: new[] { FhHealthChecksBuilder.UrlType.ExternalApi.ToString() })
             .AddAzureKeyVault(new Uri(keyVaultUrl), keyVaultCredentials, s => s.AddKey(keyName), name: "Azure Key Vault", failureStatus: HealthStatus.Degraded, tags: new[] { "Infrastructure" });
 
         string? notificationApiUrl = config.GetValue<string>("Notification:Endpoint");
@@ -83,7 +43,7 @@ public static class HealthCheck
             //todo: change notifications client to use host and append path
             notificationApiUrl = notificationApiUrl.Replace("/api/notify", "/api/info");
             healthCheckBuilder.AddUrlGroup(new Uri(notificationApiUrl), "Notification API", HealthStatus.Degraded,
-                new[] { UrlType.InternalApi.ToString() });
+                new[] { FhHealthChecksBuilder.UrlType.InternalApi.ToString() });
         }
 
         return services;
